f916fb9e9ccd1050a7fd3010b5aaf3c43f5648c2
Ibad Momin <ibadalimomin@gmail.com>
2024-03-24 18:24:05 -0500

    Synced with upstream repo

--- cause-connect/client/src/components/NavBar.js
+++ cause-connect/client/src/components/NavBar.js
@@ -1,35 +1,51 @@
 import React from "react";
+import "./Navbar.css";
 
 function NavBar() {
   return (
     <div className="navbar">
-      <div className="allBtns">
+      <div className="">
         <a href="/">
-          <button className="home">Home</button>
+          <button className="btn">Home</button>
         </a>
         <a href="/vd/donor">
-          <button className="btn">Donor</button>
+          <button className="btn"> VD Donor</button>
         </a>
         <a href="/vd/login">
-          <button className="btn">Login</button>
+          <button className="btn">VD Login</button>
         </a>
         <a href="/vd/profile">
-          <button className="btn">Profile</button>
+          <button className="btn">VD Profile</button>
         </a>
         <a href="/vd/signup">
-          <button className="btn">Signup</button>
+          <button className="btn">VD Signup</button>
         </a>
         <a href="/vd/volunteer">
-          <button className="btn">Volunteer</button>
+          <button className="btn">VD Volunteer</button>
         </a>
         <a href="/vd/welcome">
-          <button className="btn">Welcome</button>
+          <button className="btn">VD Welcome</button>
         </a>
-        <a href="/np/signup">
-          <button className="btn">Nonprofit Signup</button>
+        <a href="/np/donor">
+          <button className="btn"> NP Donor</button>
         </a>
         <a href="/np/login">
-          <button className="btn">Nonprofit Login</button>
+          <button className="btn">NP Login</button>
+        </a>
+        <a href="/np/profile">
+          <button className="btn">NP Profile</button>
+        </a>
+        <a href="/np/signup">
+          <button className="btn">NP Signup</button>
+        </a>
+        <a href="/np/volunteer">
+          <button className="btn">NP Volunteer</button>
+        </a>
+        <a href="/np/welcome">
+          <button className="btn">NP Welcome</button>
+        </a>
+        <a href="/*">
+          <button className="btn">Error</button>
         </a>
       </div>
     </div>
