9e2566a1694d90f5c07807ecad383132b28c3401
vvyn <vicavyn3@gmail.com>
2024-03-28 21:21:22 -0500

    add tailwindcss and footer

---
+++ cause-connect/client/src/components/Footer.js
@@ -0,0 +1,20 @@
+export default function Footer() {
+  return (
+    <div className="bg-orange-300 flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-blue-gray-50 py-6 text-center md:justify-between">
+      <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
+        <li>
+            About Us
+        </li>
+        <li>
+            License
+        </li>
+        <li>
+            Contribute
+        </li>
+        <li>
+            Contact Us
+        </li>
+      </ul>
+    </div>
+  );
+}
