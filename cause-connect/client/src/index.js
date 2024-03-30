22057b264ee2ba040c6d0b6d0ddb972566148226
vvyn <vicavyn3@gmail.com>
2024-03-30 16:33:41 -0500

    Revert "Merge branch 'ibad-changes' into main"
    This reverts commit c3693debfd8d976bb382eba80c2ea655de289b88, reversing
    changes made to a8fcb84ffb0c9bb9df9773a00ce0f48359693fcf.

--- cause-connect/client/src/index.js
+++ cause-connect/client/src/index.js
@@ -18,7 +18,7 @@ import NPVolunteer from './pages/NonProfit/Volunteer';
 import NPDonor from './pages/NonProfit/Donor';
 import Error from "./pages/Error";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
