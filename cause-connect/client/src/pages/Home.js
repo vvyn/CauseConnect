a3330abfc054184f2b6fc377c43b9121b5937587
vvyn <vicavyn3@gmail.com>
2024-04-02 14:18:32 -0500

    home changes

--- cause-connect/client/src/pages/Home.js
+++ cause-connect/client/src/pages/Home.js
@@ -2,8 +2,34 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      <p>Hi</p>
+    <div className="">
+      <div className="pt-20 pb-6 flex items-center justify-center text-6xl text-orange-400">CauseConnect</div>
+      <div className="flex items-center justify-center">
+        <button className="bg-orange-400 p-2  px-6 rounded-3xl text-white text-sm">Volunteer / Donor Portal</button>
+        <button className="ml-4 bg-orange-400 p-2 px-6 rounded-3xl text-white text-sm">Organization Portal</button>
+      </div>
+      
+      <div className="py-4">
+        <div className="font-bold">What We Do</div>
+        <p>Give back with us</p>
+        <p>Explore with us</p>
+        <p>CauseConnect is the ultimate platform connecting volunteers and donors with nonprofit organizations. With features like easy sign-up for volunteer shifts across multiple organizations, tracking volunteer history and donations, and facilitating communication between organizations and individuals, CauseConnect simplifies the process of making a difference. Join us today and be part of the movement for positive change.</p>
+      </div>
+
+      <div className="py-4">
+        <div className="font-bold">Who We Serve</div>
+        <p>Give back with us</p>
+        <p>Explore with us</p>
+        <p>CauseConnect is the ultimate platform connecting volunteers and donors with nonprofit organizations. With features like easy sign-up for volunteer shifts across multiple organizations, tracking volunteer history and donations, and facilitating communication between organizations and individuals, CauseConnect simplifies the process of making a difference. Join us today and be part of the movement for positive change.</p>
+      </div>
+
+      <div className="py-4">
+        <div className="font-bold">Join Us</div>
+        <p>Give back with us</p>
+        <p>Explore with us</p>
+        <p>CauseConnect is the ultimate platform connecting volunteers and donors with nonprofit organizations. With features like easy sign-up for volunteer shifts across multiple organizations, tracking volunteer history and donations, and facilitating communication between organizations and individuals, CauseConnect simplifies the process of making a difference. Join us today and be part of the movement for positive change.</p>
+      </div>
+
     </div>
   );
 }
