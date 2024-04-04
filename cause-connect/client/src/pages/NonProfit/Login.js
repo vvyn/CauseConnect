33aeec740a7b93009b86f599e41bca314317328f
Ibad Momin <ibadalimomin@gmail.com>
2024-04-04 00:15:12 -0500

    Got donor dashboard elements. Need to finish up styling so it looks bootifull.

--- cause-connect/client/src/pages/NonProfit/Login.js
+++ cause-connect/client/src/pages/NonProfit/Login.js
@@ -7,7 +7,7 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-export default function App() {
+export default function NPLogin() {
   const [email, setLoginEmail] = useState("");
   const [password, setLoginPassword] = useState("");
   const [user, setUser] = useState({});
@@ -42,48 +42,43 @@ export default function App() {
           alt="login-image"
         />
       </div>
-      <div className="ml-96 login-page">
-        <h1 className="text-4xl font-bold text-center pb-20">
-          Nonprofit Login
-        </h1>
-        <input
-          className="text-input "
-          type="email"
-          placeholder="Email"
-          onChange={(event) => {
-            setLoginEmail(event.target.value);
-          }}
-        />
-        <input
-          className="text-input"
-          type="password"
-          placeholder="Password"
-          onChange={(event) => {
-            setLoginPassword(event.target.value);
-          }}
-        />
-
-        <button className="login-button" onClick={login}>
-          {" "}
-          Login
-        </button>
-
-        <p className="text-center pt-5">
-          Don't have an account?{" "}
-          <a href="/nonprofit/signup" className="text-blue-500 underline">
-            Sign Up
-          </a>
-        </p>
-        <p className="text-center pt-5">
-          <a href="/" className="text-blue-500 underline">
-            Forgot Password?
-          </a>
-        </p>
-        <p className="text-center pt-5">
-          <a href="/" className="text-blue-500 underline">
-            Log in as an Volunteer/Donor
-          </a>
-        </p>
+      <div className="flex justify-center items-center h-screen">
+        <div className="login-page">
+          <h1 className="text-4xl font-bold pb-20">NonProfit Login</h1>
+          <input
+            className="text-input"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(event) => setLoginEmail(event.target.value)}
+          />
+          <input
+            className="text-input"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(event) => setLoginPassword(event.target.value)}
+          />
+          <button className="login-button" onClick={login}>
+            Login
+          </button>
+          <p className="text-center pt-5">
+            Don't have an account?{" "}
+            <a href="/vd/signup" className="text-blue-500 underline">
+              Sign Up
+            </a>
+          </p>
+          <p className="text-center pt-5">
+            <a href="/" className="text-blue-500 underline">
+              Forgot Password?
+            </a>
+          </p>
+          <p className="text-center pt-5">
+            <a href="/" className="text-blue-500 underline">
+              Log in as a Volunteer/Donor
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
