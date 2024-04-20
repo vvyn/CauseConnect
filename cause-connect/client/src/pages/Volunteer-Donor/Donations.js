37b727e4672f965bfe0a538ee34b912e2a2ea2de
clowie g <cdg190005@gmail.com>
2024-04-20 12:41:32 -0500

    Merge branch 'clowie-changes' of https://github.com/vvyn/CauseConnect into clowie-changes

--- cause-connect/client/src/pages/Volunteer-Donor/Donations.js
+++ cause-connect/client/src/pages/Volunteer-Donor/Donations.js
@@ -6,7 +6,7 @@ export default function Donations() {
       <div className="py-2 text-5xl text-orange-400">Name of Donation Cause</div>
       <div className="py-2">Choose the donation cause below</div>
 
-
+      
 
       <div className="flex"></div>
         <div className="mr-40">
